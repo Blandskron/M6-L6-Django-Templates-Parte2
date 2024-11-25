3f31884d8a5a60a50c2ebba785ba688cee69dfce
blandskron <bastianlandskronfreelancer@gmail.com>
2024-11-24 22:12:21 -0300

    update

---
+++ proyecto_educativo/principal/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Estudiante
+
+def home(request):
+    estudiantes = Estudiante.objects.all()
+    return render(request, 'home.html', {'estudiantes': estudiantes})
+
+def detalle_estudiante(request, estudiante_id):
+    estudiante = get_object_or_404(Estudiante, pk=estudiante_id)
+    return render(request, 'detalle.html', {'estudiante': estudiante})
